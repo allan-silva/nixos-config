8fb4f78461808f470309ea599780714c481590d7
Allan Silva <allan@allansilva.com.br>
2021-04-12 12:07:40 -0300

    Configure python docker and user groups

--- development.nix
+++ development.nix
@@ -6,5 +6,13 @@
     pkgs.git
     pkgs.vscode
     pkgs.virtualbox
+    pkgs.docker
+    pkgs.docker-compose
+    pkgs.python3
+    pkgs.python37
+    pkgs.python37Packages.virtualenv
   ];
+
+  # Docker service
+  virtualisation.docker.enable = true;
 }
--- systemwide.nix
+++ systemwide.nix
@@ -10,9 +10,9 @@
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users."allan.silva" = {
     isNormalUser = true;
-    extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
+    extraGroups = [ "wheel" "docker"]; # whell group enables ‘sudo’ for the user.
   };
 
-  # Software like vscode need set this to true
+  # Software like vscode needs set this to true
   nixpkgs.config.allowUnfree = true;
 }
